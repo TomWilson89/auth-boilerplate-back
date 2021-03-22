5222e99ea0fbd8fa5c051f7750d0debed158c859
TomWilson89 <tfuenteswilson@gmail.com>
2021-03-22 16:37:44 -0300

    Create express server Add middlewares Add prettier Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['airbnb/base', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'no-console': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/first': 'off',
+    'no-underscore-dangle': 'off',
+    'consistent-return': 'off',
+  },
+};
